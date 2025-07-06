612152231444c0c15adc9d80d1db35eaab3bc925
pushpa rajput <pr4541285@gmail.com>
2025-07-06 16:37:50 +0530

    update origin Url

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 3000;
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://quicktalk-frontend-x6r6.onrender.com",
     credentials: true,
   })
 );
